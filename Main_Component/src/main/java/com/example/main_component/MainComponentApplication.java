ce07ff6a50af94f0202ca45aac32ace73a02c03e
Marco <s0563424@htw-berlin.de>
2021-12-04 09:28:44 +0100

    dev: Anpassungen für den Main-Branch

--- Main_Component/src/main/java/com/example/main_component/MainComponentApplication.java
+++ Main_Component/src/main/java/com/example/main_component/MainComponentApplication.java
@@ -3,14 +3,11 @@ package com.example.main_component;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.UUID;
-
 @SpringBootApplication
 public class MainComponentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainComponentApplication.class, args);
     }
-
 
 }
