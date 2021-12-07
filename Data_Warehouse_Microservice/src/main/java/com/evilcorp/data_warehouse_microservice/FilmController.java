7d81eb1382d417ece2fdb9240784089a7de4e71b
Marco <s0563424@htw-berlin.de>
2021-12-07 18:38:04 +0100

    feat: FilmController GET-Request Filmliste per XML oder JSON erhalten.

--- Data_Warehouse_Microservice/src/main/java/com/evilcorp/data_warehouse_microservice/FilmController.java
+++ Data_Warehouse_Microservice/src/main/java/com/evilcorp/data_warehouse_microservice/FilmController.java
@@ -2,6 +2,8 @@ package com.evilcorp.data_warehouse_microservice;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,23 +39,46 @@ public class FilmController {
     public ResponseEntity<String> getFilmAll(
             @RequestHeader(HttpHeaders.ACCEPT) String accept) {
         log.info("getFilmAll() wird ausgeführt.");
-        MediaType mt = DataWarehouseLogik.checkAccept(accept);
+        MediaType mt = DataWarehouseLogik.checkAccept(accept); //Ueberpruefung der Akzeptierten Formate
         if(mt == null){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         List<FilmObj> filmListe = filmObjRepository.findAll();
-        ObjectMapper mapper = new ObjectMapper();
+        if(filmListe.size() == 0){
+            this.getInitial();
+            filmListe = filmObjRepository.findAll();
+        }
         String ausgabe = "";
-        try {
-            ausgabe = mapper.writeValueAsString(filmListe);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            log.error("Fehler beim Umwandeln zur JSON-Liste.");
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        HttpHeaders header = new HttpHeaders();
+        if(mt.equals(MediaType.APPLICATION_XML)) {
+            try {
+                ObjectMapper mapper = new XmlMapper();
+                header.setContentType(MediaType.APPLICATION_XML);
+                ausgabe = mapper.writeValueAsString(filmListe);
+                log.info("getFilmAll(): Saemtliche Filme werden in XML Format uebertragen.");
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+                log.error("Fehler beim Umwandeln zur JSON-Liste.");
+            }
+            //log.error("ACHTUNG XML Format ist noch nicht implementiert.");
+            //return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            //log.info("getFilmAll(): Saemtliche Filme werden in XML Format uebertragen.");
+        }
+        else {
+            try {
+                ObjectMapper mapper = new JsonMapper();
+                ausgabe = mapper.writeValueAsString(filmListe);
+                header.setContentType(MediaType.APPLICATION_JSON);
+                log.info("getFilmAll(): Saemtliche Filme werden in JSON Format uebertragen.");
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+                log.error("Fehler beim Umwandeln zur JSON-Liste.");
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
         }
         //TODO: Umwandlung in Zielformat XML oder JSON und abschicken in der Payload
         //TODO: Abschicken der Liste an Objekten
-        return new ResponseEntity<>(ausgabe, HttpStatus.OK);
+        return new ResponseEntity<>(ausgabe, header, HttpStatus.OK);
     }
 
 
