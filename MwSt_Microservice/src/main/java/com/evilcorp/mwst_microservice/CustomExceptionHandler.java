3cca7535ca790f1ce84674acec355581735ecdda
carpediem103 <s0563654@htw-berlin.de>
2021-12-07 15:16:47 +0100

    fix: die mwst-microservice komponente wurde wiederhergestellt wie sie vor der umstellung vorhanden war

--- MwSt_Microservice/src/main/java/com/evilcorp/mwst_microservice/CustomExceptionHandler.java
+++ MwSt_Microservice/src/main/java/com/evilcorp/mwst_microservice/CustomExceptionHandler.java
@@ -1,25 +1,43 @@
 package com.evilcorp.mwst_microservice;
 
+import com.evilcorp.mwst_microservice.custom_api_error.ApiError;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
-import java.io.IOException;
 
 /**
- * sorgt dafuer, dass die validation
- * einen 400 bad request code zurueck gibt,
- * wenn die validation misslingt,
- * anstatt eines 500 internal server error
+ *sammlung an exception handlern für den gesamten controller
  */
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
+    //deals with exceptions to json values that are not meeting the validation constraints
     @ExceptionHandler(ConstraintViolationException.class)
-    public void constraintViolationException(HttpServletResponse response) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex){
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+    //deals with malformed json values (e.g. typo in enum name)
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        String error = "Malformed JSON request";
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+    }
+
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 }
--- MwSt_Microservice/src/main/java/com/evilcorp/mwst_microservice/MehrwertsteuerController.java
+++ MwSt_Microservice/src/main/java/com/evilcorp/mwst_microservice/MehrwertsteuerController.java
@@ -1,120 +1,33 @@
 package com.evilcorp.mwst_microservice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.DecimalMin;
+import javax.validation.Valid;
 
 @RestController
 @Validated
 @RequestMapping("/mwst")
 public class MehrwertsteuerController {
 
-    private static final Logger log = LoggerFactory.getLogger(MehrwertsteuerController.class);
+    Logger logger = LogManager.getLogger();
 
-    /**
-     * Funktion berechnet aus den uebergebenen Preis den Mehrwert-Steuer Anteil heraus.
-     * @param price = Endpreis eines Produktes
-     * @return Mehrwertsteuer des Preises
-     */
-    @GetMapping()
-    @RequestMapping("/holen/{price}")
-    public ResponseEntity<String> cutMwStFromPrice(@PathVariable @DecimalMin(value="0.0", inclusive = false) double price){
-       double result = com.evilcorp.mwst_microservice.MehrwertsteuerCalculator.calculateMwstValue(price);
-       String outputMessage = String.valueOf(result);
-       return ResponseEntity.ok(outputMessage);
+    @GetMapping(value = "/json_request",
+                consumes = "application/json",
+                produces = "application/json")
+    @ResponseBody
+    public MehrwertsteuerModell calculateFromJSON(@Valid @RequestBody MehrwertsteuerModell mwstModell, BindingResult errors){
+        logger.info("JSON received");
+        logger.info("Calculating missing Values...");
+
+        mwstModell.calculateMissingValues();
+
+        logger.info("Values successfully calculated");
+        logger.info("Sending response JSON...");
+
+        return mwstModell;
     }
-
-    /**
-     * Funktion addiert die Mehrwertsteuer zu dem Preis.
-     * @param price = Endpreis eines Produktes
-     * @return Mehrwertsteuer des Preises
-     */
-    @GetMapping()
-    @RequestMapping("/ermitteln/{price}")
-    public ResponseEntity<String> getMwStToPrice(@PathVariable @DecimalMin(value="0.0", inclusive = false) double price){
-        double result = com.evilcorp.mwst_microservice.MehrwertsteuerCalculator.calculatePriceWithMwst(price);
-        String outputMessage = String.valueOf(result);
-        return ResponseEntity.ok(outputMessage);
-    }
-
-    /**
-     * Funktion holt den MwSt-Anteil und gibt ihn zurück
-     * @param price = Endpreis eines Produktes
-     * @return Mehrwertsteuer des Preises
-     */
-    @GetMapping()
-    @RequestMapping("/steueranteil/{price}")
-    public ResponseEntity<String> getMwStFromPrice(@PathVariable @DecimalMin(value="0.0", inclusive = false) double price){
-        double result = com.evilcorp.mwst_microservice.MehrwertsteuerCalculator.calculateMwstFromPrice(price);
-        String outputMessage = String.valueOf(result);
-        return ResponseEntity.ok(outputMessage);
-    }
-
-    /*
-    /**
-     * Funktion berechnet aus den uebergebenen Preis den Mehrwert-Steuer Anteil heraus.
-     * @param price = Endpreis eines Produktes
-     * @return Mehrwertsteuer des Preises
-     *
-    @GetMapping()
-    @RequestMapping("/holen")
-    public ResponseEntity<String> cutMwStFromPrice(@RequestBody String price){
-        try {
-            double preis = Double.parseDouble(price);
-            double erg = MehrwertsteuerCalculator.calculateMwstValue(preis);
-            String ausg = String.valueOf(erg);
-            return ResponseEntity.ok(ausg);
-        }
-        catch (NumberFormatException ex){
-            return new ResponseEntity<>("Fehlerhafter Datensatz ermittelt." , HttpStatus.BAD_REQUEST);
-        }
-    }
-
-
-    /**
-     * Funktion addiert die Mehrwertsteuer zu dem Preis.
-     * @param price = Endpreis eines Produktes
-     * @return Mehrwertsteuer des Preises
-     *
-    @GetMapping()
-    @RequestMapping("/ermitteln")
-    public ResponseEntity<String> getMwStToPrice(@RequestBody String price){
-        try {
-            double preis = Double.parseDouble(price);
-            double erg = MehrwertsteuerCalculator.calculatePriceWithMwst(preis);
-            String ausg = String.valueOf(erg);
-            return ResponseEntity.ok(ausg);
-        }
-        catch (NumberFormatException ex){
-            return new ResponseEntity<>("Fehlerhafter Datensatz ermittelt." , HttpStatus.BAD_REQUEST);
-        }
-    }
-
-
-    /**
-     * Funktion holt den MwSt-Anteil und gibt ihn zurück
-     * @param price = Endpreis eines Produktes
-     * @return Mehrwertsteuer des Preises
-     *
-    @GetMapping()
-    @RequestMapping("/steueranteil")
-    public ResponseEntity<String> getMwStFromPrice(@RequestBody String price){
-        try {
-            double preis = Double.parseDouble(price);
-            double erg = MehrwertsteuerCalculator.calculateMwstFromPrice(preis);
-            String ausg = String.valueOf(erg);
-            return ResponseEntity.ok(ausg);
-        }
-        catch (NumberFormatException ex){
-            return new ResponseEntity<>("Fehlerhafter Datensatz ermittelt." ,HttpStatus.BAD_REQUEST);
-        }
-    }
-    */
 }
