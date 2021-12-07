548be68d199004ee40be1fdfd45ee9af70a1397f
Marco <s0563424@htw-berlin.de>
2021-12-07 15:22:47 +0100

    fix: Experimental-Zugangsinformationen sind nun wieder möglich. Dependency mussten neu eingestellt werden.

--- MyFilmClasses/MyFilmClasses_Modul/src/main/java/classes/Film.java
+++ MyFilmClasses/MyFilmClasses_Modul/src/main/java/classes/Film.java
@@ -1,20 +1,25 @@
 package classes;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.persistence.*;
 import java.util.UUID;
 
-@Builder
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@SuperBuilder
 @AllArgsConstructor
+@DiscriminatorColumn(name = "type")
 public class Film {
 
     private static final Logger log = LoggerFactory.getLogger(Film.class);
 
-    @Getter
+    @Id @Getter @Setter
     protected UUID uuid_Film;
 
     public Film() {
