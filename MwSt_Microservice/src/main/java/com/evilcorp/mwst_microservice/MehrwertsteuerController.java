41a52e2adc65c46c79d9fc046d5d9ea6c840ad7a
Marco <s0563424@htw-berlin.de>
2022-01-14 17:56:27 +0100

    dev: Adjustments made for gateway connections. Further adjustments for module VAT_Microservice and MainComponent_Microservice are necessary.

--- MwSt_Microservice/src/main/java/com/evilcorp/mwst_microservice/MehrwertsteuerController.java
+++ MwSt_Microservice/src/main/java/com/evilcorp/mwst_microservice/MehrwertsteuerController.java
@@ -4,14 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
 import java.time.LocalDateTime;
 
 @RestController
