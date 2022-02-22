0a3435114ef4cb564b3303e13e684f923599ceb6
carpediem103 <s0563654@htw-berlin.de>
2022-02-22 16:00:24 +0100

    fix: request path von main/api/v1.0/mwst zu mwst/api/v1.0 geändert

--- MwSt_Microservice/src/main/java/com/evilcorp/mwst_microservice/calculator/controller/MehrwertsteuerController.java
+++ MwSt_Microservice/src/main/java/com/evilcorp/mwst_microservice/calculator/controller/MehrwertsteuerController.java
@@ -12,7 +12,7 @@ import javax.validation.Valid;
 @RestController
 @Validated
 @AllArgsConstructor
-@RequestMapping("/mwst")
+@RequestMapping("/v1.0")
 public class MehrwertsteuerController {
 
     private final MwStCalculatorService mwStCalculatorService;
