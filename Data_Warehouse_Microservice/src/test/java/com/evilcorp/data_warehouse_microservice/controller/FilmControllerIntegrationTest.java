bade8a9f0ddd408fc7da30a718eb02027ce25351
Marco <s0563424@htw-berlin.de>
2022-03-15 21:58:30 +0100

    feat: Integrationtest for getFilmAllMediaTypes() implement.

--- Data_Warehouse_Microservice/src/test/java/com/evilcorp/data_warehouse_microservice/controller/FilmControllerIntegrationTest.java
+++ Data_Warehouse_Microservice/src/test/java/com/evilcorp/data_warehouse_microservice/controller/FilmControllerIntegrationTest.java
@@ -4,10 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -20,17 +26,42 @@ public class FilmControllerIntegrationTest {
     private String path = "http://localhost:21139/film";
 
     @Test
-    void getFilmAll() throws Exception {
-        mockMvc.perform(get(path + "/all"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
     void getNewUUIDForFilmFromDataWareHouse() throws Exception {
         mockMvc.perform(get(path + "/newUuid"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")));
     }
+    @Test
+    void failureRequestsNewUUIDForFilmFromDataWareHouse() throws Exception {
+        mockMvc.perform(put(path + "/newUuid"))
+                .andExpect(status().isMethodNotAllowed());
+        mockMvc.perform(post(path + "/newUuid"))
+                .andExpect(status().isMethodNotAllowed());
+        mockMvc.perform(delete(path + "/newUuid"))
+                .andExpect(status().isMethodNotAllowed());
+    }
+
+    @Test
+    void getFilmAllMediaTypes() throws Exception {
+        String endpoint = "/all";
+
+        mockMvc.perform(get(path + endpoint)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        mockMvc.perform(get(path + endpoint)
+                        .accept(MediaType.ALL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        mockMvc.perform(get(path + endpoint)
+                        .accept(MediaType.APPLICATION_XML))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_XML));
+    }
+
+
 
     /*
     @Test
