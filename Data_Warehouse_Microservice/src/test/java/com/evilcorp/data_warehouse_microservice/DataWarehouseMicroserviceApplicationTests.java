8df6f0351ee41921ac4350d1095861938973ac5f
mh-htw-ai <72615834+mh-htw-ai@users.noreply.github.com>
2022-03-23 22:30:12 +0100

    dev: MwSt_Microservice, Main_Component_Microservice, Gateway_Microservice: Dockerfile hinzugefügt und für ein Container aufbereitet. Problem bei logback-spring.xml in MwSt_Microservice und Gateway_Microservice vorhanden, sodass bei der Ausführung über Java -jar der gebauten Anwendung die Config-Datei nicht gefunden werden kann.

--- Data_Warehouse_Microservice/src/test/java/com/evilcorp/data_warehouse_microservice/DataWarehouseMicroserviceApplicationTests.java
+++ Data_Warehouse_Microservice/src/test/java/com/evilcorp/data_warehouse_microservice/DataWarehouseMicroserviceApplicationTests.java
@@ -1,14 +1,11 @@
 package com.evilcorp.data_warehouse_microservice;
 
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootTest
-//@AutoConfigureMockMvc
-
 public class DataWarehouseMicroserviceApplicationTests {
 
     //@Autowired
--- Main_Component_Microservice/src/test/java/com/evilcorp/main_component_microservice/MainComponentMicroserviceApplicationTests.java
+++ Main_Component_Microservice/src/test/java/com/evilcorp/main_component_microservice/MainComponentMicroserviceApplicationTests.java
@@ -1,13 +1,13 @@
 package com.evilcorp.main_component_microservice;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MainComponentMicroserviceApplicationTests {
 
-    @Test
+   /* @Test
     void contextLoads() {
-    }
+    }*/
 
 }
